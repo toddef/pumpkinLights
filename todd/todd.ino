71ce1cb357e09a304c290aea94b7c3868aff855d
toddef <2912662+toddef@users.noreply.github.com>
2020-09-30 07:54:11 -0400

    split constants

--- todd/todd.ino
+++ todd/todd.ino
@@ -1,24 +1,6 @@
 #include <FastLED.h>
-
-#define LED_PIN     6
-#define COLOR_ORDER RGB
-#define CHIPSET     WS2812
-#define NUM_LEDS    30
-
-#define BRIGHTNESS  255
-#define FRAMES_PER_SECOND 60
-
-// Percentage chance to flicker or fade up/down
-#define FLICKER_CHANCE 25
-#define FADE_CHANCE 75
-
-// Range to fade brightness down
-#define BRIGHT_START 0
-#define BRIGHT_END 125
-
-// Range of intensity of the flicker 
-#define FLICKER_RANGE_LOW 5
-#define FLICKER_RANGE_HIGH 35
+  
+#include "Constants.h"
 
 CRGB leds[NUM_LEDS];
 
